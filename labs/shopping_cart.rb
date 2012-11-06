6882a80a2876074f56721465830f6f496afe722c
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-05 20:47:51 -0500

    finished shoppinh cart

--- labs/shopping_cart.rb
+++ labs/shopping_cart.rb
@@ -1,14 +1,47 @@
 require 'pry'
 
-items = {:shoes => 80.33, :shirts => 25.67, :socks => 7.99} 
+#hashes
+prices = {:shoes => 80.33, :shirts => 25.67, :socks => 7.99} 
+cart = {:shoes => 0, :shirts => 0, :socks => 0}
 
-puts "What do you want to buy? (shoes, shirts, socks, checkout)"
-what = gets.chomp
+#key variables
+tax = 0.0931
+subtotal = 0
 
-puts "How many?"
-how_many = gets.chomp
+puts "Welcome to the store!"
+puts prices
+puts "What do you want to buy? ('shoes', 'shirts', 'socks', or 'checkout')"
+input = gets.chomp
 
-price = items[what]
-cost = price * how_many
+while input != 'checkout'
 
-puts "#{what} times #{how_many} equals #{cost}"
+	puts "How many #{input} would you like to buy?"
+	how_many = gets.chomp.to_i
+
+	input = input.to_sym
+
+	#adds number of items to cart
+	cart[input] = cart[input] + how_many
+
+	#gets the total cost for items
+	cost = prices[input] * how_many
+	subtotal = subtotal + cost
+
+	#repeat question
+	puts "What do you want to buy? ('shoes', 'shirts', 'socks', or 'checkout')"
+	input = gets.chomp
+
+end
+
+tax_amount = subtotal * tax
+grand_total = subtotal + tax_amount
+
+puts "Receipt"
+puts "#{cart[:shoes]} shoes = #{cart[:shoes] * prices[:shoes]}"
+puts "#{cart[:shirts]} shirts = #{cart[:shirts] * prices[:shirts]}"
+puts "#{cart[:socks]} socks = #{cart[:socks] * prices[:socks]}"
+puts "Subtotal = #{"%.2f" % subtotal}"
+puts "Tax at 9.31% = #{"%.2f" % tax_amount}"
+puts "Grand total = #{ "%.2f" % grand_total}"
+
+
